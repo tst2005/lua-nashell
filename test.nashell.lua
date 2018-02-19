139dbe90b32d2e1de7938ecc1886bf429d96e275
TsT <tst2005@gmail.com>
2018-02-19 14:14:46 +0100

    test also with uniformapi

--- test.nashell.lua
+++ test.nashell.lua
@@ -7,12 +7,42 @@ minimal.lfs = require"lfs"
 assert(minimal.lfs.chdir)
 local S0,S = nashell(minimal)
 
-S.cd()
-print(S.pwd()==os.getenv("HOME"))
+do
+	local startpwd = S.pwd()
 
-S.cd("/tmp")
-print(S.pwd()=="/tmp")
+	S.cd()
+	print(S.pwd()==os.getenv("HOME"))
 
-for i,item in ipairs(S.ls("*")or{}) do
-	print(i,item)
+	S.cd("/tmp")
+	print(S.pwd()=="/tmp")
+
+	local r = S.ls('*')
+	table.sort(r)
+	for i,item in ipairs(r) do
+		print(i,item)
+	end
+	S.cd(startpwd) -- restore the original
+	assert(S.pwd()==startpwd)
+end
+
+do
+	local package = require"package"
+	local uniformapi = require "uniformapi"
+	local G = uniformapi({_G=_G, package=package})
+	assert(G.load)
+	S.ipairs=ipairs
+	S.print=print
+	S.table=table
+
+	local code=[[
+
+	cd '/tmp'
+	local r = ls '*'
+	table.sort(r)
+	for i,v in ipairs(r) do
+		print(i,v)
+	end
+
+	]]
+	G.load(code, code, "t", S)()
 end
